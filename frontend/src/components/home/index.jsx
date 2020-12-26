12f40a87df82050d4c6ff24ee8a6ea7007f39794
Minh <lminhphu0304@gmail.com>
2020-12-27 12:33:51 +1300

    Add basic login button in home page

---
+++ frontend/src/components/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './styles.css'
+
+export function OAuthButton() {
+    return (
+        <div className="App">
+            <h1> Login with Spotify</h1>
+            <button className="Spofity__oauth__button"> Login </button>
+        </div>
+    )    
+}
