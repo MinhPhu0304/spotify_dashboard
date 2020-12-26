aacee4e9277ea557e3acefb02797d5178c76c402
Minh <lminhphu0304@gmail.com>
2020-12-27 11:36:36 +1300

    Complete Spotify Oauth flow with set response cookie

--- server/Controllers/OAuthController.cs
+++ server/Controllers/OAuthController.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using SpotifyAPI.Web;
-using SpotifyAPI.Web.Auth;
 using static SpotifyAPI.Web.Scopes;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace server.Controllers
 {
@@ -19,36 +18,72 @@ namespace server.Controllers
         public OAuthController()
         {
             credential = CredentialSingleton.getInstance();
-            Console.WriteLine(credential.spotifyClientSecret);
         }
 
         [HttpGet]
         [Route("[controller]")]
         public bool CheckAuthRoute()
         {
+            
+            if (Request.Cookies["Test"] == null)
+            {
+                CookieOptions option = new CookieOptions();
+                option.Expires = DateTime.Now.AddMilliseconds(100);
+                Response.Cookies.Append("Test", "Bar", option);
+            }
             return true;
         }
 
         [HttpGet]
         [Route("[controller]/callback")]
-        public bool OauthCallBack(HttpContext context)
+        public async Task<bool> OauthCallBack()
         {
-            string accessToken = context.Request.Query["access_code"];
-            
-            // TODO: set cookies and redirect to authenticated page
+            string oauthCode = HttpContext.Request.Query["code"].ToString();
+            if (oauthCode != null)
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://accounts.spotify.com");
+                var content = new FormUrlEncodedContent(new[]
+                 {
+                        new KeyValuePair<string, string>("code", oauthCode),
+                        new KeyValuePair<string, string>("client_secret", credential.spotifyClientSecret),
+                        new KeyValuePair<string, string>("client_id", credential.spotifyClientId),
+                        new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                        new KeyValuePair<string, string>("redirect_uri", "https://localhost:5001/oauth/callback"),
+                     });
+                var result = await client.PostAsync("/api/token", content);
+                string resultContent = await result.Content.ReadAsStringAsync();
+                var responseJSON = JsonConvert.DeserializeObject<SpotifyTokenResponse>(resultContent);
+                SetSpotifyAccessTokenCookie(responseJSON.access_token, responseJSON.expires_in);
+            }
             return true;
         }
 
+        private void SetSpotifyAccessTokenCookie(string spotifyAccessToken, int expireInSeconds)
+        {
+            var options = new CookieOptions
+            {
+                Expires = DateTime.Now.AddSeconds(expireInSeconds),
+                IsEssential = true
+            };
+            Response.Cookies.Append("spotifyToken", spotifyAccessToken, options);
+        }
 
         [HttpGet]
         [Route("[controller]/spotify")]
         public IActionResult AuthenticateWithSpotify()
         {
-            var request = new LoginRequest(new Uri("https://localhost:5001/oauth/callback"), credential.spotifyClientId, LoginRequest.ResponseType.Token)
+            var spotifyAccessToken = HttpContext.Request.Cookies["spotifyToken"];
+            if (spotifyAccessToken == null)
             {
-                Scope = new List<string> { UserReadEmail, UserTopRead }
-            };
-            return Redirect(request.ToUri().ToString());
+                var request = new LoginRequest(new Uri("https://localhost:5001/oauth/callback"), credential.spotifyClientId, LoginRequest.ResponseType.Code)
+                {
+                    Scope = new[] { UserReadEmail, UserTopRead }
+                };
+                return Redirect(request.ToUri().ToString());
+            }
+
+            return Redirect(new Uri("https://localhost:5001/ping").ToString());
         }
     }
 }
