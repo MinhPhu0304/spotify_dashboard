2a86b5c1eafc7d10a43aeb5cab4668401bbe4f64
Minh <lminhphu0304@gmail.com>
2020-12-27 11:35:25 +1300

    Add Class definition for spotify post access token response

---
+++ server/SpotifyTokenResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server
+{
+    public class SpotifyTokenResponse
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        
+        public int expires_in { get; set; }
+
+        public string scope { get; set; }
+
+        public string refresh_token { get; set; }
+    }
+}
