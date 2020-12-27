eacb7d4f96829551ee4b642a1f6a1c21f0359d07
Minh <lminhphu0304@gmail.com>
2020-12-28 12:55:41 +1300

    Add endpoint to get top artists from backend

---
+++ server/Controllers/PersonalController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    public class PersonalController : Controller
+    {
+        [HttpGet]
+        [Route("[controller]/top_artists")]
+        public async Task<IActionResult> GetTopArtist()
+        {
+            var spotifyAccessToken = HttpContext.Request.Cookies["spotifyToken"];
+            if (spotifyAccessToken != null)
+            {
+                var spotify = new SpotifyClient(spotifyAccessToken);
+                var topArtists = await spotify.Personalization.GetTopArtists(new PersonalizationTopRequest { 
+                    Limit = 50
+                });
+                return Ok(topArtists.Items);
+            } else
+            {
+                return Forbid();
+            }
+        }
+    }
+}
