1dfb48393ba1471f400e224b73759a4d8fa19307
Minh <lminhphu0304@gmail.com>
2020-12-28 12:57:22 +1300

    Display top artist in dashboard page

--- frontend/src/components/dashboard/index.jsx
+++ frontend/src/components/dashboard/index.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
 import { useHistory } from 'react-router-dom'
 
+import { TopArtist } from './topArtists'
 import '../home/styles.css'
 
 export function Dashboard() {
     const [spotifyAccessToken, setSpotifyAccesToken] = useState('')
+    const [artistList, setArtistList] = useState([])
     const pageHistory = useHistory()
     useEffect(() => {
         const spotifyToken = Cookies.get("spotifyToken");
@@ -16,10 +18,24 @@ export function Dashboard() {
         }
     }, [])
 
+    const getTopArtists = async () => {
+        const response = await fetch("https:localhost:5001/personal/top_artists", {
+            method: 'GET',
+            credentials: 'include',
+        });
+        if (response.ok) {
+            const data = await response.json()
+            setArtistList(data)
+        } else {
+            pageHistory.push('/')
+        }
+    }
+
     return (
         <div className="App">
             View your top artists
-            <button className="Spofity__oauth__button"> View your top artist </button>
+            <button className="Spofity__oauth__button" onClick={getTopArtists}> View your top artist </button>
+            <TopArtist artistList={artistList}/>
         </div>
     )
 }
