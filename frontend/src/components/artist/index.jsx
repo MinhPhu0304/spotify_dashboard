b8ba8945eefd67d3feda1779ffb281e704ecbe10
minh <Lminhphu0304@gmail.com>
2022-05-28 10:03:34 +1200

    Redirect to login again if cookies is gone

--- frontend/src/components/artist/index.jsx
+++ frontend/src/components/artist/index.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState, useCallback } from "react";
 import Cookies from "js-cookie";
 import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export function ArtistPage() {
   let { id } = useParams();
   const [artistInfo, setArtistInfo] = useState();
+  const history = useHistory()
 
   const fetchArtist = useCallback(() => {
     return fetch(`${process.env.REACT_APP_API_ENDPOINT}/artist/${id}`, {
@@ -20,8 +22,12 @@ export function ArtistPage() {
   }, [id]);
 
   useEffect(() => {
+    if (Cookies.get("spotifyToken") == null) {
+      history.pushState("/")
+      return
+    } 
     fetchArtist();
-  }, [fetchArtist]);
+  }, [fetchArtist, history]);
 
   if (!artistInfo) {
     return null;
